36e22e9fb2ed1592549969682a7af87cf38c3860
kurizono <myukino1213@gmail.com>
2021-10-03 23:21:27 +0900

    [add]messeage

--- discordbot.py
+++ discordbot.py
@@ -17,12 +17,15 @@ async def on_command_error(ctx, error):
 #    await ctx.send('pong')
 
 async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+    if message.content == '/dice':
+        await message.channel.send('diceだよ')
+    # 開始ワード
+    if message.content.startswith('dice'):
+        # メッセージ送信者がBotだった場合は無視する
+        if message.author.bot:
+            return
+        # 「/neko」と発言したら「にゃーん」が返る処理
+        await message.channel.send('ダイスbot起動')
 
 
 
