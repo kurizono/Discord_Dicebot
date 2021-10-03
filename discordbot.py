8cebca7674d6cdd570d8045dccd5221ca0dfae60
kurizono <myukino1213@gmail.com>
2021-10-04 00:30:12 +0900

    copy

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from os import getenv
 import traceback
 from ast import parse
+import asyncio
 
 bot = commands.Bot(command_prefix='/')
 
@@ -15,17 +16,21 @@ async def on_command_error(ctx, error):
 
 @bot.event
 async def on_message(message):
-    if message.content == "/dice":
-        m = "dice"
-        # 開始ワード
-        if message.content.startswith('dice'):
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            m = "aaaa"
-        await bot.channel.send(m)
-    
-    
+    if message.content.startswith('dice'):
+        await bot.send_message(message.channel, 'this is dicebot')
 
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await bot.send_message(message.channel, 'Calculating messages...')
+        async for log in bot.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
 
+        await bot.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await bot.send_message(message.channel, 'Done sleeping')
+    
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
