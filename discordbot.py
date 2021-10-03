bdcde04919495e8cf1c53a5f690c9fe7d366df0b
kurizono <myukino1213@gmail.com>
2021-10-04 01:26:48 +0900

    str00test

--- discordbot.py
+++ discordbot.py
@@ -10,41 +10,6 @@ pattern = '\d{1,2}d\d{1,3}|\d{1,2}D\d{1,3}'
 split_pattern = 'd|D'
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.command()
-async def dice(ctx):
-    await ctx.send('1~100')
-
-@bot.event
-async def on_message(message):
-    #ちゃんとしたユーザーがメッセージ
-    if message.author == bot.user:
-        return
-    #diceから始まる
-    if message.content.startswith('dice'):
-        m = random.randint(1,100)
-        str00 = message.content.split()
-        m = nDn(str00[1])
-        await message.channel.send(m)
-    elif message.content.startswith('test'):
-        result = nDn('3d6')
-        await message.channel.send(result)
-
-token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
-
-
-
 # 対象の文字列かどうか
 def judge_nDn(src):
     repatter = re.compile(pattern)
@@ -85,3 +50,37 @@ def nDn(text):
             return 'ダイス：' + text + '\n出目：' + str(result) + '\n合計：' + str(sum_dice)
     else:
         return None
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+@bot.command()
+async def dice(ctx):
+    await ctx.send('1~100')
+
+@bot.event
+async def on_message(message):
+    #ちゃんとしたユーザーがメッセージ
+    if message.author == bot.user:
+        return
+    #diceから始まる
+    if message.content.startswith('dice'):
+        # m = random.randint(1,100)
+        str00 = message.content.split()
+        # m = nDn(str00[1])
+        await message.channel.send(str00[1])
+    elif message.content.startswith('test'):
+        result = nDn('3d6')
+        await message.channel.send(result)
+
+token = getenv('DISCORD_BOT_TOKEN')
+bot.run(token)
